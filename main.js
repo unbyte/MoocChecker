d0a05034395d943f6ec728d0eecb06601c9b020f
Desnity <i@shangyes.net>
2018-10-15 14:28:54 +0800

    optimize

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 document.body.appendChild(document.createElement('script')).src='//code.jquery.com/jquery-1.9.1.min.js';
 setTimeout(function(){
-    jQuery(".s label:last-child").each(
+    $(".s label:last-child").each(
         function(){
-            jQuery(this).click();
+            $(this).click();
         });
-    jQuery(".j-textarea").each(
+    $(".j-textarea").each(
         function(){
-            jQuery(this).val("Good Job."); //这里是点评的内容
+            $(this).val("Good Job."); //这里是点评的内容
         });
 },2000)
