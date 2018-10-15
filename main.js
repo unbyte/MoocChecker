b8eab97c62a986fea947aec2e7a255286b3e3616
Desnity <i@shangyes.net>
2018-10-15 22:17:10 +0800

    optimize

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// 请先使用以下代码
+
 document.body.appendChild(document.createElement('script')).src='//code.jquery.com/jquery-1.9.1.min.js';
 setTimeout(function(){
     $(".s label:last-child").each(
@@ -9,3 +11,17 @@ setTimeout(function(){
             $(this).val("Good Job."); //这里是点评的内容
         });
 },2000)
+
+
+
+
+// 上面的代码使用过一次以后，只要页面没有刷新，请直接使用以下代码
+
+$(".s label:last-child").each(
+    function(){
+        $(this).click();
+    });
+$(".j-textarea").each(
+    function(){
+        $(this).val("Good Job."); //这里是点评的内容
+    });
