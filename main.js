c145abe621b18ae81a7f996640f2eb7bfc60bd9e
Desnity <i@shangyes.net>
2018-10-15 14:15:34 +0800

    optimize

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
-var jqueryJs=document.createElement('script');
-jqueryJs.setAttribute("type","text/JavaScript");
-jqueryJs.setAttribute("src", 'http://lib.baomitu.com/jquery/2.2.4/jquery.min.js');
-document.getElementsByTagName('head')[0].appendChild(jqueryJs);
-jQuery(".s label:last-child").each(
-    function(){
-        jQuery(this).click();
-    });
-jQuery(".j-textarea").each(
-    function(){
-        jQuery(this).val("Good Job.");
-    });
+document.body.appendChild(document.createElement('script')).src='//code.jquery.com/jquery-1.9.1.min.js';
+setTimeout(function(){
+    jQuery(".s label:last-child").each(
+        function(){
+            jQuery(this).click();
+        });
+    jQuery(".j-textarea").each(
+        function(){
+            jQuery(this).val("Good Job.");
+        });
+},2000)
