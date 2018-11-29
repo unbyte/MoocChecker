b55bc27d9ceabd6374ca4e1fe0364d79600f5ceb
YMNNs <i@stager.top>
2018-11-29 23:31:15 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,20 +2,19 @@ let suckIt = "<div style=\"position: fixed;top: 50%;z-index: 999;right: 10%;padd
 document.body.insertAdjacentHTML("afterbegin",suckIt);
 var cbk = document.getElementById('autooo');
 function fuckIt() {
-    var element=document.getElementsByClassName("j-gotonext");
+	var element=document.getElementsByClassName("j-gotonext");
 	let judge = document.getElementById('fuckMe').value;
     [].forEach.call(document.getElementsByClassName('s'), e => {e.children[e.children.length-1].children[0].checked=true});
     [].forEach.call(document.getElementsByClassName('j-textarea'), e => {e.value=judge});
     if(cbk.checked){
-	document.getElementsByClassName('j-submitbtn')[0].click()
 	if (typeof(element)== "undefined" || element == null){
-document.getElementsByClassName('j-gotonext')[0].click()
-}else{
-alert("Completed.");
-return false;
-}
+		return false;
+	}
+	else{
+		document.getElementsByClassName('j-submitbtn')[0].click()
+		document.getElementsByClassName('j-gotonext')[0].click()
+	}
 	setTimeout(fuckIt, 1000);
-}
-
+	}
 	return false;
-}
+	}
